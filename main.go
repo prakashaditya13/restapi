2b3b1e08a1ab65e2b66fba2d5735b7b2a9a31354
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-05-19 11:07:25 +0530

    listen Server

--- main.go
+++ main.go
@@ -15,4 +15,10 @@ func main() {
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/book/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/books", createBooks).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBooks).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBooks).Methods("DELETE")
+	// Run a Server
+	http.ListenAndServe(":8000", r)
 }
